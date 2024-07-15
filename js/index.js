418d5696422256e1ed1436e1885e050a4d0940ac
Huy <zzzdarkdeadzzz@gmail.com>
2024-07-16 01:22:52 +0700

    lần 2

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ moon.addEventListener('click', function () {
     body.classList.toggle('bg-dark');
 });
 
-
+//get and render ds từ 
 async function getValueOnMock() {
     try {
         let resolve = await axios({
@@ -49,12 +49,12 @@ function renderDSPet(arr) {
                     <img src="${img}" alt="">
                 </div>
                 <div class="pet_info">
-                    <p id="id">Mã sp: ${id}</p>
+                    <span>Mã SP: <span id="id">${id}</span></span>
                     <h4 id="name">${name}</h4>
                     <p id="gia" class="giamGia">${price}</p>
                 </div>
                 <div class="d-flex justify-content-end">
-                    <button type="button" class="buyBtn">Mua</button>
+                    <button type="button" onclick="muaSP(${id},${price})" class="buyBtn">Thêm vào giỏ</button>
                 </div>`
         if (bestSale) {
             content += `
@@ -69,6 +69,98 @@ function renderDSPet(arr) {
             content += `</div>`
         }
     }
-    console.log(content)
-    document.querySelector(`.list_pet`).innerHTML=content
+    document.querySelector(`.list_pet`).innerHTML = content
 }
+
+let DSMua = []
+let count = 0
+let priceBill = 0
+//btn buy
+function muaSP(idSP, price) {
+    let index = DSMua.findIndex(item => item.id === idSP)
+    if (index !== -1) {
+        DSMua[index].inCart += 1
+        count += 1
+    } else {
+        DSMua.push({
+            id: idSP,
+            inCart: 1,
+            price
+        })
+        count += 1
+    }
+    console.log(DSMua)
+    document.querySelector(`#soLuongDaChon`).innerHTML = `${count}`
+}
+
+//btn thanh toán
+const payBill= async () => {
+    let content = ""
+    priceBill = 0
+    try {
+        for (let [index, item] of DSMua.entries()) {
+            const result = await axios({
+                url: `https://6680c8e056c2c76b495cbc78.mockapi.io/product/${item.id}`,
+                method: "GET"
+            })
+
+            let { img, desc, price } = result.data
+            content += `
+            <tr>
+                <td class="STT">${index + 1}</td>
+                <td>
+                    <img src="${img}" alt="Hình ảnh mô tả sản phẩm" width="100px">
+                </td>
+                <td>
+                    <div class="colDesc">
+                        <span>Id: ${item.id}</span>
+                        <span>${desc}</span>
+                        <p>Price: ${price}</p>
+                    </div>
+                </td>
+                <td>
+                    <div class="colSoLuong" >
+                        <input type="text" name="soLuong" id="soLuong${index}" value="${item.inCart}" style="width: 40px;">
+                        <div class="changeSoLuong ">
+                            <button type="button" class="btn btn-primary" onclick="addSP('${index}',-1)">-</button>
+                            <button type="button" class="btn btn-primary" onclick="addSP('${index}')">+</button>
+                        </div>
+                    </div>
+                </td>
+            </tr>`
+            priceBill += price * item.inCart
+        }
+    }
+    catch (error) {
+        console.log(error)
+
+    }
+    document.querySelector(`#billInfo`).innerHTML = content
+    document.querySelector(`#tongTien`).innerHTML = priceBill
+}
+
+document.querySelector(`#payCart`).onclick = payBill
+
+const addSP = (index, soLuong = 1) => {
+    DSMua[index].inCart += soLuong
+
+    if (DSMua[index].inCart < 0) {
+        let result = confirm("Bạn không thích bé này ư?");
+        if (result) {
+            DSMua.splice(index,1)
+            console.log(DSMua)
+            payBill()
+        } else {
+            DSMua[index].inCart += 1
+        }
+    } else {
+        count += soLuong * 1
+        document.querySelector(`#soLuongDaChon`).innerHTML = count
+        document.querySelector(`#soLuong${index}`).value = DSMua[index].inCart
+        priceBill += DSMua[index].price * soLuong
+        document.querySelector(`#tongTien`).innerHTML = priceBill
+    }
+}
+
+
+
