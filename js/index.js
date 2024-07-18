48344362464ec1f53a10e27e94db89443d4b25ae
Huy <zzzdarkdeadzzz@gmail.com>
2024-07-18 23:26:55 +0700

    test

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ const sun = document.querySelector('.gg-sun');
 const moon = document.querySelector('.gg-moon');
 const body = document.querySelector('body');
 
-// sun.addEventListener('click', function () {
-//     this.classList.toggle('active');
-//     moon.classList.toggle('active');
-//     body.classList.toggle('bg-dark');
-// });
-// moon.addEventListener('click', function () {
-//     sun.classList.toggle('active');
-//     this.classList.toggle('active');
-//     body.classList.toggle('bg-dark');
-// });
+sun.addEventListener('click', function () {
+    this.classList.toggle('active');
+    moon.classList.toggle('active');
+    body.classList.toggle('bg-dark');
+});
+moon.addEventListener('click', function () {
+    sun.classList.toggle('active');
+    this.classList.toggle('active');
+    body.classList.toggle('bg-dark');
+});
 
 
 
